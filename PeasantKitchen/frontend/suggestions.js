fcc7f499f13739b7a3901f6a2a9fb95d995103d0
JoshRodgers <joshrodge@gmail.com>
2016-10-12 17:15:08 -0700

    user entries now displayed

--- PeasantKitchen/frontend/suggestions.js
+++ PeasantKitchen/frontend/suggestions.js
@@ -21,4 +21,37 @@ suggestedIngred.forEach(function(item){
    option.value = item;
    list.appendChild(option);
 });
-}//]]> 
+}//]]>
+
+function createTable() {
+    var table = document.getElementById("myTable");
+    var row = table.insertRow(0);
+    var cell1 = row.insertCell(0);
+		var entry = document.getElementById("foods").value;
+    cell1.innerHTML = entry;
+}
+
+
+/*
+function createTable()
+{
+    var num_rows = document.getElementById('rows').value;
+    var num_cols = document.getElementById('cols').value;
+    var theader = '<table border="1">\n';
+    var tbody = '';
+
+    for( var i=0; i<num_rows;i++)
+    {
+        tbody += '<tr>';
+        for( var j=0; j<num_cols;j++)
+        {
+            tbody += '<td>';
+            tbody += 'Cell ' + i + ',' + j;
+            tbody += '</td>'
+        }
+        tbody += '</tr>\n';
+    }
+    var tfooter = '</table>';
+    document.getElementById('wrapper').innerHTML = theader + tbody + tfooter;
+}
+*/
